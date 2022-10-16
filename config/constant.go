617ea85b4eb341e171003fd243ea1cccb5c7da0d
khalidrianda <khalidrianda12@gmail.com>
2022-10-16 23:48:24 +0700

    config

---
+++ config/constant.go
@@ -0,0 +1,9 @@
+package config
+
+const (
+	DUPLICATED_DATA = "error, data duplicate in database"
+	DATABASE_ERROR  = "error, database could not process"
+	ENCRYPT_ERROR   = "error, cant encrypt the password"
+	REJECTED_DATA   = "error, rejected from database"
+	NO_DATA         = "no data in database"
+)
