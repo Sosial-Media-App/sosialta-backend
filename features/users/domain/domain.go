ce096fcda62a9b06df089f795f7b5639c8bec2fd
khalidrianda <khalidrianda12@gmail.com>
2022-10-18 11:26:03 +0700

    domain

---
+++ features/users/domain/domain.go
@@ -0,0 +1,31 @@
+package domain
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	ID          uint
+	Username    string
+	Email       string
+	Fullname    string
+	Phone       string
+	Dob         string
+	UserPicture string
+}
+
+type Repository interface {
+	Insert(newUser Core) (Core, error)
+	Login(newUser Core) (Core, string, error)
+	Update(newUser Core) (Core, error)
+	Delete(newUser Core) (Core, error)
+}
+
+type Services interface {
+	AddUser(newUser Core) (Core, error)
+	Login(newUser Core) (Core, error)
+	UpdateUser(newUser Core) (Core, error)
+	DeleteUser(newUser Core) error
+}
+
+type Handler interface {
+	RegiterUser() echo.HandlerFunc
+}
