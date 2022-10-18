4808e7ee626b2be1b892e9b014ded5ce145763b9
khalidrianda <khalidrianda12@gmail.com>
2022-10-18 15:49:23 +0700

    edit main

--- server.go
+++ server.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"github.com/Sosial-Media-App/sosialta/config"
+	uDelivery "github.com/Sosial-Media-App/sosialta/features/users/delivery"
+	uRepo "github.com/Sosial-Media-App/sosialta/features/users/repository"
+	uServices "github.com/Sosial-Media-App/sosialta/features/users/services"
+	"github.com/Sosial-Media-App/sosialta/utils/database"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
+func main() {
+	e := echo.New()
+	cfg := config.NewConfig()
+	db := database.InitDB(cfg)
+	uRepo := uRepo.New(db)
+
+	uServices := uServices.New(uRepo)
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	uDelivery.New(e, uServices)
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
