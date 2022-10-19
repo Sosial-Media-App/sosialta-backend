71eaad4bb8f9c891566023b2b4ad03065d66c15d
khalidrianda <khalidrianda12@gmail.com>
2022-10-19 21:39:14 +0700

    request and response

--- features/comments/delivery/response.go
+++ features/comments/delivery/response.go
@@ -1,1 +1,54 @@
 package delivery
+
+import "github.com/Sosial-Media-App/sosialta/features/comments/domain"
+
+func SuccessResponse(msg string, data interface{}) map[string]interface{} {
+	return map[string]interface{}{
+		"message": msg,
+		"data":    data,
+	}
+}
+
+func FailedResponse(msg string) map[string]string {
+	return map[string]string{
+		"message": msg,
+	}
+}
+
+type UpdateResponse struct {
+	ID        uint   `json:"id"`
+	IdUser    uint   `json:"id_user"`
+	IdContent uint   `json:"id_content"`
+	Comment   string `json:"comment"`
+}
+
+func ToResponse(core interface{}, code string) interface{} {
+	var res interface{}
+	switch code {
+	case "update":
+		cnv := core.(domain.Core)
+		res = UpdateResponse{
+			ID: cnv.ID, IdUser: cnv.IdUser, IdContent: cnv.IdContent,
+			Comment: cnv.Comment,
+		}
+	case "register":
+		cnv := core.(domain.Core)
+		res = UpdateResponse{
+			ID: cnv.ID, IdUser: cnv.IdUser, IdContent: cnv.IdContent,
+			Comment: cnv.Comment,
+		}
+	}
+	return res
+}
+
+func ToResponseContent(core interface{}, code string) interface{} {
+	var res interface{}
+	var arr []UpdateResponse
+	val := core.([]domain.Core)
+	for _, cnv := range val {
+		arr = append(arr, UpdateResponse{ID: cnv.ID, IdUser: cnv.IdUser, IdContent: cnv.IdContent,
+			Comment: cnv.Comment})
+	}
+	res = arr
+	return res
+}
