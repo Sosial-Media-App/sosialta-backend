ce10ccac962acbda6b2a88f0cbe5419b56b5f6a2
muhdjau <mhmd.jauhari@gmail.com>
2022-10-21 11:01:15 +0700

    fixed users testing

---
+++ features/comments/services/services_test.go
@@ -0,0 +1,129 @@
+package services
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/Sosial-Media-App/sosialta/config"
+	"github.com/Sosial-Media-App/sosialta/features/comments/domain"
+	mocks "github.com/Sosial-Media-App/sosialta/mocks/comments"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetComment(t *testing.T) {
+	repo := mocks.NewRepository(t)
+	returnData := []domain.Core{{
+		ID: 1, IdUser: 1, IdContent: 1, Comment: "Pertamax gan, cendolnya dong!",
+	}}
+
+	t.Run("Sukses Get Comment", func(t *testing.T) {
+		repo.On("Get", mock.Anything).Return(returnData, nil).Once()
+		srv := New(repo)
+		input := uint(1)
+		res, err := srv.GetComment(input)
+		assert.NotNil(t, res)
+		assert.Nil(t, err)
+		assert.Equal(t, returnData, res)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Database Error", func(t *testing.T) {
+		repo.On("Get", mock.Anything).Return(nil, errors.New(config.DATABASE_ERROR)).Once()
+		srv := New(repo)
+		input := uint(0)
+		res, err := srv.GetComment(input)
+		assert.Nil(t, res)
+		assert.Nil(t, err)
+		assert.EqualError(t, errors.New(config.DATABASE_ERROR), config.DATABASE_ERROR)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("No Data", func(t *testing.T) {
+		repo.On("Get", mock.Anything).Return(nil, errors.New(config.NO_DATA)).Once()
+		srv := New(repo)
+		input := uint(0)
+		res, err := srv.GetComment(input)
+		assert.Nil(t, res)
+		assert.Nil(t, err)
+		assert.EqualError(t, errors.New(config.NO_DATA), config.NO_DATA)
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestAddComment(t *testing.T) {
+	repo := mocks.NewRepository(t)
+	returnData := domain.Core{
+		ID: 1, IdUser: 1, IdContent: 1, Comment: "Pertamax gan, cendolnya dong!"}
+
+	t.Run("Sukses Add Comment", func(t *testing.T) {
+		repo.On("Insert", mock.Anything).Return(returnData, nil).Once()
+		srv := New(repo)
+		input := domain.Core{IdContent: 1, Comment: "Chronoptic Energy!"}
+		res, err := srv.AddComment(input)
+		assert.NotNil(t, res)
+		assert.Nil(t, err)
+		assert.Equal(t, returnData, res)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Duplicated Data", func(t *testing.T) {
+		repo.On("Insert", mock.Anything).Return(domain.Core{}, errors.New(config.DUPLICATED_DATA)).Once()
+		srv := New(repo)
+		input := domain.Core{IdContent: 1, Comment: "Chronoptic Energy!"}
+		res, err := srv.AddComment(input)
+		assert.NotNil(t, res)
+		assert.NotNil(t, err)
+		assert.EqualError(t, errors.New(config.DUPLICATED_DATA), config.DUPLICATED_DATA)
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestUpdateComment(t *testing.T) {
+	repo := mocks.NewRepository(t)
+	returnData := domain.Core{ID: 1, IdUser: 1, IdContent: 1, Comment: "Pertamax gan, cendolnya dong!"}
+
+	t.Run("Sukses Update Comment", func(t *testing.T) {
+		repo.On("Update", mock.Anything, mock.Anything).Return(returnData, nil).Once()
+		srv := New(repo)
+		input := domain.Core{IdContent: 1, Comment: "Chronoptic Energy!"}
+		res, err := srv.UpdateComment(input)
+		assert.Nil(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, returnData, res)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Rejected from Database", func(t *testing.T) {
+		repo.On("Update", mock.Anything).Return(domain.Core{}, errors.New(config.REJECTED_DATA)).Once()
+		srv := New(repo)
+		input := domain.Core{}
+		res, err := srv.UpdateComment(input)
+		assert.Equal(t, res, domain.Core{})
+		assert.NotNil(t, err)
+		assert.EqualError(t, errors.New(config.REJECTED_DATA), config.REJECTED_DATA)
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestDeleteComment(t *testing.T) {
+	repo := mocks.NewRepository(t)
+	input := uint(1)
+	t.Run("Sukses Delete Comment", func(t *testing.T) {
+		repo.On("Delete", mock.Anything).Return(nil).Once()
+		srv := New(repo)
+		err := srv.DeleteComment(input)
+		assert.Nil(t, err)
+		assert.NoError(t, err)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Data Not Found", func(t *testing.T) {
+		repo.On("Delete", mock.Anything).Return(errors.New(config.NO_DATA)).Once()
+		srv := New(repo)
+		err := srv.DeleteComment(uint(0))
+		assert.NotNil(t, err)
+		assert.EqualError(t, errors.New(config.NO_DATA), config.NO_DATA)
+		repo.AssertExpectations(t)
+	})
+}
---
+++ features/users/services/services_test.go
@@ -0,0 +1,183 @@
+package services
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/Sosial-Media-App/sosialta/config"
+	"github.com/Sosial-Media-App/sosialta/features/users/domain"
+	mocks "github.com/Sosial-Media-App/sosialta/mocks/users"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetUser(t *testing.T) {
+	repo := mocks.NewRepository(t)
+	returnData := domain.Core{
+		ID: 1, Fullname: "Elon Musk", Username: "elonmusk", Email: "elon@tesla.com",
+		Password: "qwerty12345", Phone: "08123456789", Dob: "1988-08-19", UserPicture: "www.cryptotimes.io/wp-content/uploads/2022/03/8817.jpg"}
+
+	t.Run("Sukses Get User", func(t *testing.T) {
+		repo.On("Get", mock.Anything).Return(returnData, nil).Once()
+		srv := New(repo)
+		res, err := srv.GetUser(returnData)
+		assert.NotNil(t, res)
+		assert.Nil(t, err)
+		assert.Equal(t, returnData, res)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Database Error", func(t *testing.T) {
+		repo.On("Get", mock.Anything).Return(domain.Core{}, errors.New(config.DATABASE_ERROR)).Once()
+		srv := New(repo)
+		res, err := srv.GetUser(domain.Core{})
+		assert.NotNil(t, res)
+		assert.Nil(t, err)
+		assert.EqualError(t, errors.New("error, database could not process"), config.DATABASE_ERROR)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("No Data", func(t *testing.T) {
+		repo.On("Get", mock.Anything).Return(domain.Core{}, errors.New(config.NO_DATA)).Once()
+		srv := New(repo)
+		res, err := srv.GetUser(domain.Core{})
+		assert.NotNil(t, res)
+		assert.Nil(t, err)
+		assert.EqualError(t, errors.New("no data in database"), config.NO_DATA)
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestAddUser(t *testing.T) {
+	repo := mocks.NewRepository(t)
+	returnData := domain.Core{
+		ID: 1, Fullname: "Elon Musk", Username: "elonmusk", Email: "elon@tesla.com", Password: "qwerty12345"}
+
+	t.Run("Sukses Insert User", func(t *testing.T) {
+		repo.On("Insert", mock.Anything).Return(returnData, nil).Once()
+		srv := New(repo)
+		input := domain.Core{
+			Fullname: "Elon Musk", Username: "elonmusk", Email: "elon@tesla.com", Password: "qwerty12345"}
+		res, err := srv.AddUser(input)
+		assert.Nil(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, returnData, res)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Encrypt Error", func(t *testing.T) {
+		repo.On("Insert", mock.Anything).Return(domain.Core{}, errors.New(config.ENCRYPT_ERROR)).Once()
+		srv := New(repo)
+		input := domain.Core{}
+		res, err := srv.AddUser(input)
+		assert.NotNil(t, res)
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, config.ENCRYPT_ERROR)
+		assert.Equal(t, domain.Core{}, res)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Duplicated Data", func(t *testing.T) {
+		repo.On("Insert", mock.Anything).Return(domain.Core{}, errors.New(config.DUPLICATED_DATA)).Once()
+		srv := New(repo)
+		input := domain.Core{}
+		res, err := srv.AddUser(input)
+		assert.NotNil(t, res)
+		assert.NotNil(t, err)
+		assert.EqualError(t, errors.New("error, data duplicate in database"), config.DUPLICATED_DATA)
+		assert.Equal(t, domain.Core{}, res)
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestLogin(t *testing.T) {
+	repo := mocks.NewRepository(t)
+	returnData := domain.Core{ID: 1, Fullname: "Elon Musk", Username: "elonmusk", Email: "elon@tesla.com", Password: "$2a$10$40zy5ax5eMNDo/SSVmTem.5eqT0bu350nkE6qxEhpOLGgXQ4DXtcS"}
+
+	t.Run("Sukses Login", func(t *testing.T) {
+		repo.On("Login", mock.Anything).Return(returnData, nil).Once()
+		srv := New(repo)
+		input := domain.Core{Email: "elon@tesla.com", Password: "qwerty12345"}
+		res, err := srv.Login(input)
+		assert.Nil(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, returnData, res)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Database Error", func(t *testing.T) {
+		repo.On("Login", mock.Anything).Return(domain.Core{}, errors.New(config.DATABASE_ERROR)).Once()
+		srv := New(repo)
+		res, err := srv.Login(domain.Core{})
+		assert.NotNil(t, res)
+		assert.NotNil(t, err)
+		assert.EqualError(t, errors.New("error, database could not process"), config.DATABASE_ERROR)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("No Data", func(t *testing.T) {
+		repo.On("Login", mock.Anything).Return(domain.Core{}, errors.New(config.NO_DATA)).Once()
+		srv := New(repo)
+		res, err := srv.Login(domain.Core{})
+		assert.NotNil(t, res)
+		assert.NotNil(t, err)
+		assert.EqualError(t, errors.New("no data in database"), config.NO_DATA)
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestUpdateUser(t *testing.T) {
+	repo := mocks.NewRepository(t)
+	returnData := domain.Core{
+		ID: 1, Fullname: "Elon Musk", Username: "elonmusk", Email: "elon@tesla.com", Password: "qwerty12345",
+		Phone: "08123456789", Dob: "1988-08-19", UserPicture: "www.cryptotimes.io/wp-content/uploads/2022/03/8817.jpg"}
+
+	t.Run("Sukses Update User", func(t *testing.T) {
+		repo.On("Update", mock.Anything, mock.Anything).Return(returnData, nil).Once()
+		srv := New(repo)
+		input := domain.Core{
+			Fullname: "Elon Musk", Username: "elonmusk", Email: "elon@tesla.com", Password: "qwerty12345",
+			Phone: "08123456789", Dob: "1988-08-19", UserPicture: "www.cryptotimes.io/wp-content/uploads/2022/03/8817.jpg"}
+		res, err := srv.UpdateUser(input, input.ID)
+		assert.Nil(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, returnData, res)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Rejected from Database", func(t *testing.T) {
+		repo.On("Update", mock.Anything).Return(domain.Core{}, errors.New(config.REJECTED_DATA)).Once()
+		srv := New(repo)
+		input := domain.Core{
+			Fullname: "Elon Musk", Username: "elonmusk", Email: "elon@tesla.com", Password: "qwerty12345",
+			Phone: "08123456789", Dob: "1988-08-19", UserPicture: "www.cryptotimes.io/wp-content/uploads/2022/03/8817.jpg"}
+		res, err := srv.UpdateUser(input, uint(0))
+		assert.Equal(t, res, domain.Core{})
+		assert.NotNil(t, err)
+		assert.EqualError(t, errors.New(config.REJECTED_DATA), config.REJECTED_DATA)
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestDeleteUser(t *testing.T) {
+	repo := mocks.NewRepository(t)
+	input := uint(1)
+
+	t.Run("Sukses Delete User", func(t *testing.T) {
+		repo.On("Delete", mock.Anything).Return(nil).Once()
+		srv := New(repo)
+		err := srv.DeleteUser(input)
+		assert.Nil(t, err)
+		assert.NoError(t, err)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Data Not Found", func(t *testing.T) {
+		repo.On("Delete", mock.Anything).Return(errors.New(config.NO_DATA)).Once()
+		srv := New(repo)
+		err := srv.DeleteUser(uint(0))
+		assert.NotNil(t, err)
+		assert.EqualError(t, errors.New(config.NO_DATA), config.NO_DATA)
+		repo.AssertExpectations(t)
+	})
+}
